8fd35c32e16ab30c040152f4e684438d7e2bf8a9
agj60 <gaidys@uol.com.br>
2024-10-03 12:03:45 -0300

    Cleaned up setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,4 @@ setup(
     name = 'colab_lib_manager',
     version = '0.1.0',
     description = 'A library to manage installation of Python packages in Google Colab using Google Drive',
-    #long_description = open('READ.md').read(),
-    #long_description_content_type = 'text/markdown',
-    #author = 'agj60',
-    #url = 'https://github.com/agj60/colab_lib_manager',
-    #packages = find_packages(),
-    #install_requires = [
-    #    'sys', 'subprocess', 'os', 'importlib', 'google.colab',
-    #],
 )
