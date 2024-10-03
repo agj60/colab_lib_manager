0d1931abbcd4e730e666ff0a2b8c6ecd0d39657c
agj60 <gaidys@uol.com.br>
2024-10-03 12:05:25 -0300

    re-included find_packeages in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup #, find_packages
+from setuptools import setup, find_packages
 
 setup(
     name = 'colab_lib_manager',
     version = '0.1.0',
     description = 'A library to manage installation of Python packages in Google Colab using Google Drive',
+    packages = find_packages(),
 )
